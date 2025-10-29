3e0c8641a8bd7061628313cd8bc54e69ab2519fd
Jerry <jerrydu524@gmail.com>
2025-10-29 15:10:06 -0400

    return to 6 am working

--- app.py
+++ app.py
@@ -311,8 +311,8 @@ scheduler = BackgroundScheduler()
 scheduler.add_job(
     func=scheduled_scraper,
     trigger="cron",
-    hour=20,
-    minute=24,
+    hour=6,
+    minute=0,
     id='daily_scraper'
 )
 
