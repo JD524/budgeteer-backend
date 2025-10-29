c5b0a7698880b16c432696aee9ea6093dbd0ec14
Jerry <jerrydu524@gmail.com>
2025-10-29 19:12:57 -0400

    test new schedule with aldi

--- app.py
+++ app.py
@@ -311,8 +311,8 @@ scheduler = BackgroundScheduler()
 scheduler.add_job(
     func=scheduled_scraper,
     trigger="cron",
-    hour=6,
-    minute=0,
+    hour=11,
+    minute=15,
     id='daily_scraper'
 )
 
