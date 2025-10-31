cb752ab2f4fa864ea96c65df49c8aeb518c95bd8
Jerry <jerrydu524@gmail.com>
2025-10-31 19:40:11 -0400

    return to midnight

--- app.py
+++ app.py
@@ -331,8 +331,8 @@ scheduler = BackgroundScheduler()
 scheduler.add_job(
     func=scheduled_scraper,
     trigger="cron",
-    hour=23,
-    minute=39,
+    hour=4,
+    minute=0,
     id='daily_scraper'
 )
 scheduler.start()
